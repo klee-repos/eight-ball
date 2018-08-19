0f741d7c0a5ec01e0da5915c74c89fffe2ff0cf7
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-08-18 19:44:04 -0700

    Initial Commit

---
+++ Eight Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eight Ball
+//
+//  Created by Kevin Lee on 8/18/18.
+//  Copyright © 2018 Kevin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
