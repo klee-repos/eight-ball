3bd76b7e658f11dd8499bf308e1256aec7c49763
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-08-18 20:02:14 -0700

    first commit

--- Eight Ball/ViewController.swift
+++ Eight Ball/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomNumber : Int = 0
 
+    @IBOutlet weak var BallView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        randomizeBall()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressAskbutton(_ sender: Any) {
+        randomizeBall()
+    }
+    
+    func randomizeBall() {
+        let eightBall = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+        randomNumber = Int(arc4random_uniform(5))
+        BallView.image = UIImage(named: eightBall[randomNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        randomizeBall()
+    }
+    
 }
 
